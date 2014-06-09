c6211e8cd86d624e41d70ead4f318f35391f54f6
Matthew Joyce <matsjoyce@gmail.com>
2014-06-09 10:23:23 +0100

    Expose more useful functions from dill

--- safe_dill/__init__.py
+++ safe_dill/__init__.py
@@ -1,10 +1,12 @@
 from .dill_helper import save, load, set_getsattr, unset_getsattr
 from .safe_dill import set_safe_modules, init
-from .dill import loads, dumps, _trace as debug
+from .dill import loads, dumps, _trace as debugfrom .dill import loads, dumps, _trace as debug, register, copy, \
+    PicklingError, UnpicklingError
 
 init()
 
-__all__ = ["save", "load", "set_safe_modules", "loads", "dumps", "debug"]
+__all__ = ["save", "load", "set_safe_modules", "loads", "dumps", "debug",
+           "register", "copy", "PicklingError", "UnpicklingError"]
 
 from sandbox import on_start, on_end
 on_start(set_getsattr)
