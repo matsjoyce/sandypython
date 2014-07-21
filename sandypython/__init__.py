8a11f6a0b3f07104261ef33e33c7846a7fe71acc
Matthew Joyce <matsjoyce@gmail.com>
2014-07-21 20:34:12 +0100

    Add checks for python version

--- sandypython/__init__.py
+++ sandypython/__init__.py
@@ -1,3 +1,13 @@
+import sys
+
+if sys.version_info[0] < 3:
+    raise RuntimeError("SandyPython only works for python 3")
+
+import warnings
+
+if sys.version_info.minor < 4:
+    warnings.warn("SandyPython has only been tested for python 3.4")
+
 from . import core, utils, spec, safe_dill
 
 utils.clean_module(core)
